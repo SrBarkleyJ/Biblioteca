8beced2ce316f6ece14c30df77023aa8902359ef
SrBarkleyJ <jorgexdddd@gmail.com>
2025-11-10 13:52:32 +0100

    Update contenedor2.ts

--- src/app/contenedor2/contenedor2.ts
+++ src/app/contenedor2/contenedor2.ts
@@ -4,14 +4,14 @@ import { B2 } from "../b2/b2";
 import { B3 } from "../b3/b3";
 import { B4 } from "../b4/b4";
 import { B5 } from "../b5/b5";
-import { B6 } from "../b6/b6";
 import {B7} from "../b7/b7"
 @Component({
   selector: 'app-contenedor2',
-  imports: [B1, B2, B3, B4, B5, B6, B7],
+  imports: [B1, B2, B3, B4, B5, B7],
   templateUrl: './contenedor2.html',
   styleUrl: './contenedor2.css',
 })
 export class Contenedor2 {
 
 }
+
