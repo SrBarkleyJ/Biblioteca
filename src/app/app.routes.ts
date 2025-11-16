832a5ad4a4ad116e7d576f70bf6afda26c8a7f86
SrBarkleyJ <jorgexdddd@gmail.com>
2025-11-16 15:28:10 +0100

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Contenedor1 } from './contenedor1/contenedor1';
 import { Contenedor2 } from './contenedor2/contenedor2';
 import { Contenedor3 } from './contenedor3/contenedor3';
 import { Contenedor4 } from './contenedor4/contenedor4';
+import { Contenedor5 } from './contenedor5/contenedor5';
 
 export const routes: Routes = [
     {path:'', component:Contenedor1},
-    {path:'contenedor1', component:Contenedor1},
-    {path:'contenedor2', component:Contenedor2},
-    {path: 'contenedor3', component: Contenedor3},
-    {path: 'contenedor4', component: Contenedor4}
+    {path:'cards', component:Contenedor1},
+    {path:'redes', component:Contenedor2},
+    {path: 'loading', component: Contenedor3},
+    {path: 'login', component: Contenedor4},
+    {path: 'experimental', component: Contenedor5}
 ];
